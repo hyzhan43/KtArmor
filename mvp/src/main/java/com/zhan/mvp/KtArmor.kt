d81bcdccc5efaff596d0fc2e3a9eaf0fd9220398
HyZhan <hyzhan43@163.com>
2019-06-04 17:57:08 +0800

    modify KtArmor init function

--- mvp/src/main/java/com/zhan/mvp/KtArmor.kt
+++ mvp/src/main/java/com/zhan/mvp/KtArmor.kt
@@ -9,32 +9,26 @@ import com.zhan.mvp.config.Setting
  *  @date:   2019/5/22
  *  @desc:   TODO
  */
-class KtArmor private constructor(builder: Builder) {
 
-    companion object {
-        lateinit var BASE_URL: String
-        var CONNECT_TIME_OUT: Long = 0
-        var READ_TIME_OUT: Long = 0
-        var WRITE_TIME_OUT: Long = 0
-        inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
-    }
+object KtArmor {
 
-    init {
-        BASE_URL = builder.baseUrl
-        CONNECT_TIME_OUT = builder.connectTime
-        READ_TIME_OUT = builder.readTime
-        WRITE_TIME_OUT = builder.writeTime
+    var BASE_URL: String = ""
+    var CONNECT_TIME_OUT: Long = 0
+    var READ_TIME_OUT: Long = 0
+    var WRITE_TIME_OUT: Long = 0
+
+    fun init(context: Context,
+             baseUrl: String,
+             connectTime: Long = Setting.CONNECT_TIME_OUT,
+             readTime: Long = Setting.READ_TIME_OUT,
+             writeTime: Long = Setting.WRITE_TIME_OUT) {
+
+        BASE_URL = baseUrl
+        CONNECT_TIME_OUT = connectTime
+        READ_TIME_OUT = readTime
+        WRITE_TIME_OUT = writeTime
+
         // 初始化 SharePreference
-        Preference.init(builder.context)
-    }
-
-    class Builder {
-        lateinit var context: Context
-        lateinit var baseUrl: String
-        var connectTime = Setting.CONNECT_TIME_OUT
-        var readTime = Setting.READ_TIME_OUT
-        var writeTime = Setting.WRITE_TIME_OUT
-
-        fun build() = KtArmor(this)
+        Preference.init(context)
     }
 }
