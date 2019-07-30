0751a4085fbaed9fd539d6067c44cb976b201e44
HyZhan <hyzhan43@163.com>
2019-07-30 17:18:17 +0800

    add KtArmor-Plugin

---
+++ app/src/main/java/com/zhan/hy/ktarmor/register/RegisterContract.kt
@@ -0,0 +1,18 @@
+package com.zhan.hy.ktarmor.register
+
+import com.zhan.mvp.mvp.BaseContract
+
+/**
+ *  @author: hyzhan
+ *  @date:   2019/07/30
+ *  @desc:   TODO
+ */
+
+interface RegisterContract {
+
+    interface View : BaseContract.View{
+    }
+
+    interface Presenter : BaseContract.Presenter {
+    }
+}
---
+++ app/src/main/java/com/zhan/hy/ktarmor/register/RegisterModel.kt
@@ -0,0 +1,13 @@
+package com.zhan.hy.ktarmor.register
+
+import com.zhan.mvp.mvp.BaseModel
+
+/**
+ *  @author: hyzhan
+ *  @date:   2019/07/30
+ *  @desc:   TODO
+ */
+
+object RegisterModel: BaseModel(){
+
+}
