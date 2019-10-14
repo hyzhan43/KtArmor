fc2f0b7ad66f91412c8cdf37c5fe4d9a33de45a9
HyZhan <hyzhan43@163.com>
2019-10-14 16:00:40 +0800

    beautiful BasePresenter quickLaunch code

--- mvp/src/main/java/com/zhan/mvp/mvp/BasePresenter.kt
+++ mvp/src/main/java/com/zhan/mvp/mvp/BasePresenter.kt
@@ -38,38 +38,6 @@ abstract class BasePresenter<V : BaseContract.View>(view: V) : BaseContract.Pres
         Execute<R>().apply(block)
     }
 
-    inner class Execute<R> {
-
-        private var successBlock: ((R?) -> Unit)? = null
-        private var failBlock: ((String?) -> Unit)? = null
-        private var exceptionBlock: ((Throwable?) -> Unit)? = null
-
-        fun request(block: suspend CoroutineScope.() -> KResponse<R>?) {
-            launchUI({
-                block()?.run {
-                    if (isSuccess()) {
-                        successBlock?.invoke(getKData())
-                        return@run
-                    }
-
-                    failBlock?.invoke(getKMessage()) ?: showError(getKMessage())
-                }
-            }, exceptionBlock)
-        }
-
-        fun onSuccess(block: (R?) -> Unit) {
-            this.successBlock = block
-        }
-
-        fun onFail(block: (String?) -> Unit) {
-            this.failBlock = block
-        }
-
-        fun onException(block: (Throwable?) -> Unit) {
-            this.exceptionBlock = block
-        }
-    }
-
     private fun showException(exception: String) {
         exception.showLog()
         // TODO toast 网络异常
@@ -79,9 +47,10 @@ abstract class BasePresenter<V : BaseContract.View>(view: V) : BaseContract.Pres
     fun <R> KResponse<R>.execute(success: (R?) -> Unit, error: ((String) -> Unit)? = null) {
         if (this.isSuccess()) {
             success(this.getKData())
-        } else {
-            error?.invoke(this.getKMessage()) ?: showError(this.getKMessage())
+            return
         }
+
+        error?.invoke(this.getKMessage()) ?: showError(this.getKMessage())
     }
 
     override fun showError(msg: String) {
@@ -100,5 +69,29 @@ abstract class BasePresenter<V : BaseContract.View>(view: V) : BaseContract.Pres
         mViewRef.clear()
         // 取消掉 presenterScope创建的所有协程和其子协程。
         presenterScope.cancel()
+    }
+
+
+    inner class Execute<R> {
+
+        private lateinit var successBlock: ((R?) -> Unit)
+        private var failBlock: ((String?) -> Unit)? = null
+        private var exceptionBlock: ((Throwable?) -> Unit)? = null
+
+        fun request(block: suspend CoroutineScope.() -> KResponse<R>?) {
+            launchUI({ block()?.execute(successBlock, failBlock) }, exceptionBlock)
+        }
+
+        fun onSuccess(block: (R?) -> Unit) {
+            this.successBlock = block
+        }
+
+        fun onFail(block: (String?) -> Unit) {
+            this.failBlock = block
+        }
+
+        fun onException(block: (Throwable?) -> Unit) {
+            this.exceptionBlock = block
+        }
     }
 }
